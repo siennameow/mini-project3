36088a842f4dced3ae95986cd4901955e5a9da6e
Hexuan Li <lihexuan1@gmail.com>
2022-03-30 12:15:23 -0700

    add html and javascript basic

---
+++ script.js
@@ -0,0 +1,12 @@
+var userPick = ["R", "P", "S"];
+var computerPick = ["R", "P", "S"];
+
+
+function myFunction() {
+    let start = alert("Let's play RPS!");
+    let action1 = prompt("Make your choice");
+    
+    let result1 = alert("You lost, the computer wins!");
+}
+
+
